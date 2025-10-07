8afef3f9238e55a315964d7ad2f9f12f27e17b27
loliththomas <loliththomas@gmail.com>
2025-10-07 16:10:06 +0530

    some changes

--- src/Components/DashboardOverview/DashboardOverview.tsx
+++ src/Components/DashboardOverview/DashboardOverview.tsx
@@ -11,6 +11,20 @@ export function DashboardOverview() {
         <StatCard title="Pre-Booked" totalSlots={50} currentStatSlots={10} />
         <StatCard title="Named" totalSlots={50} currentStatSlots={2} />
       </div>
+      <div className="stats-card-container">
+        <StatCard title="Total" totalSlots={50} />
+        <StatCard title="Available" totalSlots={50} currentStatSlots={8} />
+        <StatCard title="Booked" totalSlots={50} currentStatSlots={3} />
+        <StatCard title="Pre-Booked" totalSlots={50} currentStatSlots={10} />
+        <StatCard title="Named" totalSlots={50} currentStatSlots={2} />
+      </div>
+      <div className="stats-card-container">
+        <StatCard title="Total" totalSlots={50} />
+        <StatCard title="Available" totalSlots={50} currentStatSlots={8} />
+        <StatCard title="Booked" totalSlots={50} currentStatSlots={3} />
+        <StatCard title="Pre-Booked" totalSlots={50} currentStatSlots={10} />
+        <StatCard title="Named" totalSlots={50} currentStatSlots={2} />
+      </div>
       
       
     </div>
