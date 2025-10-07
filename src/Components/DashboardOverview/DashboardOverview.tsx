4c37879086774a6e95ad410fc5f00f8c2155df5a
loliththomas <loliththomas@gmail.com>
2025-10-07 15:45:20 +0530

    dashboard updated

--- src/Components/DashboardOverview/DashboardOverview.tsx
+++ src/Components/DashboardOverview/DashboardOverview.tsx
@@ -1,7 +1,18 @@
-export function DashboardOverview(){
-    return(
-        <div>
-            Dashboard Content
-        </div>
-    )
+import { StatCard } from "../StatsCard/StatsCard";
+import "../DashboardOverview/dashboardOverview.css";
+
+export function DashboardOverview() {
+  return (
+    <div className="dashboard-overview">
+      <div className="stats-card-container">
+        <StatCard title="Total" totalSlots={50} />
+        <StatCard title="Available" totalSlots={50} currentStatSlots={8} />
+        <StatCard title="Booked" totalSlots={50} currentStatSlots={3} />
+        <StatCard title="Pre-Booked" totalSlots={50} currentStatSlots={10} />
+        <StatCard title="Named" totalSlots={50} currentStatSlots={2} />
+      </div>
+      
+      
+    </div>
+  );
 }
